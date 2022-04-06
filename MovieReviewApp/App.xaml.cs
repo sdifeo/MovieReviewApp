99ec91cef5657f4378a26bae2fe7b5dc695f7b1b
mkhoabui <mkhoabui@gmail.com>
2022-04-05 21:45:59 -0400

    change xamarin project

--- MovieReviewApp/App.xaml.cs
+++ MovieReviewApp/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using MovieReviewApp.Services;
-using MovieReviewApp.Views;
-using System;
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,13 +6,11 @@ namespace MovieReviewApp
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
