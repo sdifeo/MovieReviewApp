e9cefc81f67679bf7c420891a463d280d8ed33c1
steven <s.difeo2@gmail.com>
2022-04-05 21:28:22 -0400

    first commit

---
+++ MovieReviewApp/MovieReviewApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MovieReviewApp.Services;
+using MovieReviewApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieReviewApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MovieReviewApp/MovieReviewApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MovieReviewApp.Models;
+using MovieReviewApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieReviewApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
