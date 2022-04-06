6563c659fc757489e5de0e3a48896cdf32cae341
steven <s.difeo2@gmail.com>
2022-04-06 00:32:53 -0400

    created signin and signup pages

--- MovieReviewApp/App.xaml.cs
+++ MovieReviewApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace MovieReviewApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new SignIn();
         }
 
         protected override void OnStart()
