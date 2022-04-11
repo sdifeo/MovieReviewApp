6bf93dc2687275be281ebb5e41c0c5f7da8f7713
steven <s.difeo2@gmail.com>
2022-04-10 20:34:02 -0400

    added create review page and buttons to carousel page

--- MovieReviewApp/CarouselPage.xaml.cs
+++ MovieReviewApp/CarouselPage.xaml.cs
@@ -15,11 +15,28 @@ namespace MovieReviewApp
         public CarouselPage()
         {
             InitializeComponent();
+
+            if (ReviewCarousel.ItemsSource == null)
+            {
+                ReviewCarousel.RemoveLogicalChild(this);
+
+                Label label = new Label();
+                label.Text = "Please add a review";
+                label.HorizontalTextAlignment = TextAlignment.Center;
+                label.FontSize = 25;
+                label.TextColor = Color.Black;
+                MainStack.Children.Add(label);
+            }
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
 
+        }
+
+        private void GoToCreateReview(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreateReviewPage());
         }
     }
 }
---
+++ MovieReviewApp/CreateReviewPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieReviewApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateReviewPage : ContentPage
+    {
+        public CreateReviewPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SaveReviewBtn(object sender, EventArgs e)
+        {
+            var score = Review_Score.Text;
+            var title = Movie_Title.Text;
+
+            if (score == null)
+            {
+                DisplayAlert("Score", "Please select a score", "OK");
+            }
+
+            if (title == null)
+            {
+                DisplayAlert("Title", "Please enter a movie title", "OK");
+            }
+        }
+    }
+}
